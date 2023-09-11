cf1d764796131e512af8c0bd1bbc29d3da64a7a3
su-mrak <ariaotp@gmail.com>
2023-09-11 08:25:32 +0300

    wtf

--- backend/presentation/app.py
+++ backend/presentation/app.py
@@ -1,21 +1,22 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from presentation import users_router, hacks_router, teams_router, publications_router
-
-
 def create_app() -> FastAPI:
     fastapi_app = FastAPI()
 
     fastapi_app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
+        allow_origins=[
+            "https://example.com",  # Replace with your allowed origins
+            "https://another-domain.com",
+        ],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
     )
+
+    # Include your routers as you were doing before
     fastapi_app.include_router(users_router.router)
     fastapi_app.include_router(teams_router.router)
     fastapi_app.include_router(hacks_router.router)
     fastapi_app.include_router(publications_router.router)
 
     return fastapi_app
+
