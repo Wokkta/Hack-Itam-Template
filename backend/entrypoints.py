8fadb8ea60595edec38b4ceb67f7c088297accbf
su-mrak <ariaotp@gmail.com>
2023-09-11 05:39:17 +0000

    update app

--- backend/entrypoints.py
+++ backend/entrypoints.py
@@ -1,3 +1,13 @@
-from presentation.app import create_app
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from presentation.users_router import router as users_router  # Import users_router
+from presentation.teams_router import router as teams_router
+from presentation.hacks_router import router as hacks_router
+from presentation.publications_router import router as publications_router
 
 app = create_app()
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- backend/presentation/app.py
+++ backend/presentation/app.py
@@ -1,22 +1,21 @@
+from fastapi import FastAPI  # Import the FastAPI class
+from fastapi.middleware.cors import CORSMiddleware
+
+# Your other imports and code here...
+
 def create_app() -> FastAPI:
     fastapi_app = FastAPI()
 
     fastapi_app.add_middleware(
         CORSMiddleware,
-        allow_origins=[
-            "https://example.com",  # Replace with your allowed origins
-            "https://another-domain.com",
-        ],
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
     )
-
-    # Include your routers as you were doing before
     fastapi_app.include_router(users_router.router)
     fastapi_app.include_router(teams_router.router)
     fastapi_app.include_router(hacks_router.router)
     fastapi_app.include_router(publications_router.router)
 
     return fastapi_app
-
