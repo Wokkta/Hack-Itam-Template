32c6f49d92e4e983745910e0c862745a9d4b6a17
su-mrak <81665747+su-mrak@users.noreply.github.com>
2023-09-11 13:42:47 +0300

    Delete frontend directory

--- frontend/src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import hackatonsReducer from './Slices/hackatonsSlice';
-import userReducer from './Slices/userSlice';
-export const store = configureStore({
-  reducer: {
-    hackatons: hackatonsReducer,
-    user: userReducer,
-  },
-});
